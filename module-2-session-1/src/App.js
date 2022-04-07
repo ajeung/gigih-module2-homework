017bb2179ed267dfe2313ae1271c083c30378979
Ajeung Angsaweni <ajeung11@gmail.com>
2022-04-08 03:53:52 +0700

    update

--- module-2-session-1/src/App.js
+++ module-2-session-1/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Record from './pages/home/index.js';
+import createPlaylist from './components/create-playlist';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Record />
+        <createPlaylist />
       </header>
     </div>
   );
---
+++ module-2-session-1/src/components/create-playlist/index.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+class createPlaylist extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {playlist: []};
+    }
+
+    render() {
+        return (
+            <div class="newSong">
+                <h1>Create Playlist</h1>
+                <form name="songForm">
+                    <label>Title :</label><br />
+                    <input type="text" id="title" /><br /><br />
+                    <label>Artist :</label><br />
+                    <input type="text" id="artist" /><br /><br />
+                    <label>Album :</label><br />
+                    <input type="text" id="album" /><br /><br />
+                    <button id="btn">Add</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default createPlaylist;
