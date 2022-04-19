23cbbcf8cbc66250165edefcbc9f279405bc0636
Ajeung Angsaweni <ajeung11@gmail.com>
2022-04-20 05:54:48 +0700

    homework module-6-session-1

--- module-2-session-1/src/App.test.js
+++ module-2-session-1/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+test('renders homepage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const playlistDisplay = screen.getByText(/Playlist/i);
+  const selectButton = screen.getByText('SELECT');
+  expect(playlistDisplay).toBeInTheDocument();
+  expect(selectButton).toBeInTheDocument();
 });
